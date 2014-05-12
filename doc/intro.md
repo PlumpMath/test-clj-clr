f3b2fb5c87b47a7020f8e29f6affcf94fe668e31
timsgardner <timsgardner@gmail.com>
2014-05-12 17:26:13 -0400

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to test-clj-clr
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/test_clj_clr/core_test.clj
@@ -0,0 +1,7 @@
+(ns test-clj-clr.core-test
+  (:use clojure.test
+        test-clj-clr.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
