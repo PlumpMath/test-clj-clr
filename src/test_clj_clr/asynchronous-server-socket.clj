b0ec9c2f3683e6085be051fd5c247edf0d0daa14
timsgardner <timsgardner@gmail.com>
2014-06-19 16:49:40 -0400

    Just changed indentation, but now seem to have confirmation that the CLR namespaces load at least

--- src/test_clj_clr/asynchronous-server-socket.clj
+++ src/test_clj_clr/asynchronous-server-socket.clj
@@ -1,5 +1,10 @@
 (ns test-clj-clr.core
   (:use clojure.repl clojure.pprint)
   (:import [System.Net.Sockets
-            Socket SocketType ProtocolType SocketShutdown] ;DM: (java.net Socket ServerSocket InetSocketAddress)
-           [System.Net IPAddress IPEndPoint]))
+            Socket
+            SocketType
+            ProtocolType
+            SocketShutdown] ;DM: (java.net Socket ServerSocket InetSocketAddress)
+           [System.Net
+            IPAddress
+            IPEndPoint]))
