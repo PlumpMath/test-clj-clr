d8e0d01b2acb1dfe49cd641021b420897e704e45
timsgardner <timsgardner@gmail.com>
2014-06-20 02:46:16 -0400

    making good progress

--- src/test_clj_clr/asynchronous-server-socket.clj
+++ src/test_clj_clr/asynchronous-server-socket.clj
@@ -1,10 +1,99 @@
 (ns test-clj-clr.core
-  (:use clojure.repl clojure.pprint)
+  (:refer-clojure :exclude [send])
+  (:require [test-clj-clr.utils :as u])
+  (:use clojure.repl
+        clojure.pprint
+        [clojure.reflect :only [reflect]]
+        [clojure.walk :only [prewalk postwalk]])
   (:import [System.Net.Sockets
             Socket
             SocketType
             ProtocolType
-            SocketShutdown] ;DM: (java.net Socket ServerSocket InetSocketAddress)
+            SocketShutdown
+            AddressFamily]
            [System.Net
+            Dns
+            IPHostEntry
             IPAddress
-            IPEndPoint]))
+            IPEndPoint]
+           [System.Threading
+            ManualResetEvent]))
+
+(do 
+  (set! *print-length* 20)
+  (set! *print-level* 20))
+
+;; StateObject. ----------------------------------------------
+
+(defrecord StateObject
+    [^Socket work-socket
+     ^int buffer-size
+     ^bytes buffer
+     ^StringBuilder sb])
+
+(defn make-StateObject []
+  (let [bs (int 1024)]
+    (map->StateObject
+      {:work-socket nil
+       :buffer-size bs
+       :buffer (byte-array bs)
+       :sb (StringBuilder.)})))
+
+;; listener --------------------------------------
+
+;; weird all-done thing 
+(def ^ManualResetEvent all-done (ManualResetEvent. false))
+
+(defn send-callback [])
+
+(defn send []
+  :bla)
+
+(defn read-callback)
+
+(defn accept-callback [^IAsyncResult ar]
+  ;;Signal the main thread to continue
+  (.Set all-done)
+  (let [^Socket listener (ar.AsyncState) ;; still weirds me, see line 74 in example
+        ^Socket handler (.EndAccept listener ar)
+        ^StateObject state ( (make-StateObject)
+                             (set! ))] 
+    ))
+
+(defn get-local-end-point []
+  (let [^IPAddress ipAddress (->
+                               (Dns/GetHostName)
+                               (Dns/GetHostByName)
+                               .AddressList
+                               first)]
+    (IPEndPoint. ipAddress, 11000)))
+
+(defn get-listener []
+  (Socket. AddressFamily/InterNetwork,
+    SocketType/Stream,
+    ProtocolType/Tcp))
+
+(def kill-switch (atom false))
+
+(defn start-listening []
+  (let [^IPEndPoint local-end-point (get-local-end-point)
+        ^Socket listener (get-listener)]
+    (try
+      (doto listener
+        (.Bind local-end-point)
+        (.Listen 100))
+
+      (while (not @kill-switch) ;;supposed to be infinitish
+        (.Reset all-done)
+        (println "Waiting for connection...")
+        (.BeginAccept socket
+          (AsyncCallback. accept-callback)
+          listener)
+        (.WaitOne all-done))
+
+      (catch Exception e
+        (println (.ToString e))))
+    (println "\nPress ENTER to continue...")
+    (println "now I guess I need to tell it to continue somehow")
+    (println "see loc 65 in example")))
+
